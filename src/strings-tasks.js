b86c38df2384f39ae5e21c6d178a07a33eade267
anakdrch <ana.kudrevich@gmail.com>
2024-12-26 16:20:52 +0100

    feat: implement reverseString

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -263,8 +263,12 @@ function formatTime(minutes, seconds) {
  *   reverseString('abcdef') => 'fedcba'
  *   reverseString('12345') => '54321'
  */
-function reverseString(/* str */) {
-  throw new Error('Not implemented');
+function reverseString(str) {
+  let newStr = '';
+  for (let i = str.length - 1; i >= 0; i -= 1) {
+    newStr += str.charAt(i);
+  }
+  return newStr;
 }
 
 /**
