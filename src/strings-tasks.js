9b40841a4069ecbb65b1cb5ceacb4b8ae3dd94cd
anakdrch <ana.kudrevich@gmail.com>
2024-12-26 13:05:31 +0100

    feat: repeatString

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -134,8 +134,9 @@ function removeTrailingWhitespaces(value) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  if (times >= 0) return str.repeat(times);
+  return '';
 }
 
 /**
