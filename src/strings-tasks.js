83bd7889c6bf99a12af610bb187cc96624237a95
anakdrch <ana.kudrevich@gmail.com>
2024-12-26 12:43:40 +0100

    feat: implement isString

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -38,8 +38,11 @@ function getStringLength(value) {
  *   isString('test') => true
  *   isString(new String('test')) => true
  */
-function isString(/* value */) {
-  throw new Error('Not implemented');
+function isString(value) {
+  if (typeof value === 'string' || value instanceof String) {
+    return true;
+  }
+  return false;
 }
 
 /**
